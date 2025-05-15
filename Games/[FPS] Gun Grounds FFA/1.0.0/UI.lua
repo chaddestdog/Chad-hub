a6afab8a6bc015a0a286bdb804402b984492877e
chaddestdog <remove6868@gmail.com>
2025-05-15 03:15:35 -0400

    Update UI.lua
    Bruh vars broke

--- Games/[FPS] Gun Grounds FFA/1.0.0/UI.lua
+++ Games/[FPS] Gun Grounds FFA/1.0.0/UI.lua
@@ -1,12 +1,10 @@
 _G.Chad = {aimbot = {aimbot = {enabled = false, psilet = false, vischeck = false}; triggerbot = {enabled = false}}};
 repeat task.wait() until _G.Chad_2 and _G.Chad_2.loaded;
 
-do -- vars 
-    local Library = loadstring(game:HttpGet("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))();
-    local Window = Library:CreateWindow{Title = "Chad hub" ,SubTitle = "[FPS] Gun Grounds FFA",TabWidth = 160,Size = UDim2.fromOffset(830, 525),Resize = true,MinSize = Vector2.new(470, 380),Acrylic = false, Theme = "Dark",MinimizeKey = Enum.KeyCode.RightControl};
-    local Tabs = {aimbot = Window:CreateTab{Title = "aimbot", Icon = ""}, visuals = Window:CreateTab{Title = "visuals", Icon = ""}, misc = Window:CreateTab{Title = "misc", Icon = ""}};
-    local flags = _G.Chad;
-end;
+local Library = loadstring(game:HttpGet("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))();
+local Window = Library:CreateWindow{Title = "Chad hub" ,SubTitle = "[FPS] Gun Grounds FFA",TabWidth = 160,Size = UDim2.fromOffset(830, 525),Resize = true,MinSize = Vector2.new(470, 380),Acrylic = false, Theme = "Dark",MinimizeKey = Enum.KeyCode.RightControl};
+local Tabs = {aimbot = Window:CreateTab{Title = "aimbot", Icon = ""}, visuals = Window:CreateTab{Title = "visuals", Icon = ""}, misc = Window:CreateTab{Title = "misc", Icon = ""}};
+local flags = _G.Chad;
 
 do -- Tabs.aimbot 
     -- aimbot
