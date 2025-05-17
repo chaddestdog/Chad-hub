a9eb4e0d569ab6cd001539738734fe1c94bac1d5
chaddestdog <remove6868@gmail.com>
2025-05-17 09:41:05 -0400

    Update UI.lua

--- Games/[FPS] Gun Grounds FFA/1.0.0/UI.lua
+++ Games/[FPS] Gun Grounds FFA/1.0.0/UI.lua
@@ -1,9 +1,12 @@
-_G.Chad = {aimbot = {aimbot = {enabled = false, psilet = false, vischeck = false}; triggerbot = {enabled = false}}};
+_G.Chad = {aimbot = {aimbot = {enabled = false, psilet = false, vischeck = false}; triggerbot = {enabled = false}};
+    misc = {
+        anti_aim = {enabled = false};
+    };};
 repeat task.wait() until _G.Chad_2 and _G.Chad_2.loaded;
 
 -- vars --------------------------------------------------------
     local Library = loadstring(game:HttpGet("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))();
-    local Window = Library:CreateWindow{Title = "Chad hub" ,SubTitle = "[FPS] Gun Grounds FFA",TabWidth = 160,Size = UDim2.fromOffset(830, 525),Resize = true,MinSize = Vector2.new(470, 380),Acrylic = false, Theme = "Dark",MinimizeKey = Enum.KeyCode.RightControl};
+    local Window = Library:CreateWindow{Title = "Modest hook" ,SubTitle = "[FPS] Gun Grounds FFA",TabWidth = 160,Size = UDim2.fromOffset(830, 525),Resize = true,MinSize = Vector2.new(470, 380),Acrylic = false, Theme = "Dark",MinimizeKey = Enum.KeyCode.RightControl};
     local Tabs = {aimbot = Window:CreateTab{Title = "aimbot", Icon = ""}, visuals = Window:CreateTab{Title = "visuals", Icon = ""}, misc = Window:CreateTab{Title = "misc", Icon = ""}};
     local flags = _G.Chad;
     
@@ -15,3 +18,6 @@ repeat task.wait() until _G.Chad_2 and _G.Chad_2.loaded;
        
     -- trigger bot
     Tabs.aimbot:CreateToggle("d", {Title = "triggerbot", Default = false , Callback = function (val) flags.aimbot.triggerbot.enabled = val end});
+    
+    -- anti aim 
+    Tabs.misc:CreateToggle("e", {Title = "anti aim", Default = false , Callback = function (val) flags.misc.anti_aim.enabled = val end});
